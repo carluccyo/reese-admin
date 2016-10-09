2ff6a867dceec959849d2608aa4ccb814ae82a85
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-10-09 22:51:44 +0200

    setup simple-line-icons

--- src/vendor.ts
+++ src/vendor.ts
@@ -17,10 +17,4 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import 'font-awesome/css/font-awesome.css';
-
-// import 'font-awesome/fonts/FontAwesome.otf';
-// import 'font-awesome/fonts/fontawesome-webfont.eot';
-// import 'font-awesome/fonts/fontawesome-webfont.svg';
-// import 'font-awesome/fonts/fontawesome-webfont.ttf';
-// import 'font-awesome/fonts/fontawesome-webfont.woff';
-// import 'font-awesome/fonts/fontawesome-webfont.woff2';
+import 'simple-line-icons/css/simple-line-icons.css';
